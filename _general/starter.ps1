d0b0d910ea2ff2052c82c1f02f983810e720d54f
Philippe Crama <pcfeb0009@gmx.com>
2018-02-19 16:04:48 +0100

    Improve after installation on 2nd computer

--- _general/starter.ps1
+++ _general/starter.ps1
@@ -1,46 +1,68 @@
 Function Start-OnceOnly {
     param (
-	    [string]$shim,
-		[int]$delay
-	)
+        [string]$shim,
+        [int]$delay
+    )
     try {
-	    $_ = Get-Process $shim -ErrorAction Stop
-		Write-Host "$shim already running"
+        $_ = Get-Process $shim -ErrorAction Stop
+        Write-Host "$shim already running"
     } catch {
-		Write-Host "Starting $shim"
-		Start-Sleep $delay
-	    . $shim
-	}
+        Write-Host "Starting $shim"
+        Start-Sleep $delay
+        . $shim
+    }
 }
 
 Function Wait-Process {
     param (
-	    [string]$shim
-	)
+        [string]$shim
+    )
     $stop = (Get-Date).AddMinutes(3)
     while ((Get-Date) -lt $stop) {
         try {
             $_ = Get-Process $shim -ErrorAction Stop
-			Write-Output "$shim OK..."
+            Write-Output "$shim OK..."
             Break
         } catch {
             Start-Sleep 5
-			Write-Output "$shim not started?"
+            Write-Output "$shim not started?"
         }
     }
 }
 
-$basename = "ConEmu64"
+$scriptName = & { $myInvocation.ScriptName }
+$Env:SCOOP = Split-Path -Path (Split-Path -Path (Split-Path -Path (Resolve-Path $scriptName) -Parent) -Parent) -Parent
+Write-Host "SCOOP=`"$($Env:SCOOP)`""
+$Env:PATH = ($Env:PATH).Replace("%SCOOP%", $Env:SCOOP)
+Write-Host "Initial Path=$($Env:PATH)"
+
+$Env:HOME = $Env:HOMEDRIVE + $Env:HOMEPATH
+$opt = $Env:HOME + "\opt"
+$Env:PATH = (
+    "$opt\emaxw64\bin" +
+    ";$opt\bin" +
+    ";$($Env:HOME)\AppData\Roaming\Python\Scripts" +
+    ";$opt\vasco\DigipassSequencer-20170519_2_3_6_2_QA" +
+    ";$opt\vasco\DPEmulator_1_0_5_0_forVC_3_15_0" +
+    ";$opt\vasco\DpxDumpPro" +
+    ";$opt\vasco\bin" +
+    ";" + $Env:PATH)
+
+$basename = "ConEmu64.exe"
 $conemu = "$(Resolve-Path (scoop which "$basename"))"
-$scoop = "$(Resolve-Path "$(Split-Path -Path "$conemu" -Parent)\..\..\..")"
+$scoop = $Env:SCOOP
 $ssh_agent = "$scoop\apps\git\current\usr\bin\ssh-agent.exe"
+Write-Host "SCOOP=`"$($Env:SCOOP)`"`nbasename=`"$basename`"`nconemu=`"$conemu`"`nscoop=`"$scoop`"`nssh_agent=`"$ssh_agent`""
 Start-Process -WindowStyle Hidden -FilePath "$ssh_agent" -ArgumentList @("$conemu", "-LoadCfgFile", "$($scoop)\persist\conemu\conemu.xml")
 Wait-Process bash
 
-$Env:HOME=$Env:HOMEDRIVE + $Env:HOMEPATH
-
 # Start other programs with a certain delay...
-Foreach ($x in (@("greenshot", 10), @("ditto", 10), @("multicommander", 10), @("keypirinha", 10))) {
+Foreach ($x in (@("flux", 10),
+                @("workrave", 10),
+                @("greenshot", 10),
+                @("ditto", 10),
+                @("multicommander", 10),
+                @("keypirinha", 10))) {
     Start-OnceOnly $x[0] $x[1]
     Wait-Process $x[0]
 }
