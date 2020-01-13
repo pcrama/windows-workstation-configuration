45cc64a26ea4bf76753c5bab64bcca737b25ceed
Philippe Crama <pcfeb0009@gmx.com>
2020-01-13 11:38:22 +0000

    Finish removing conemu, see also fdb3567e1dc1e6bf

--- _general/makeshortcut.ps1
+++ _general/makeshortcut.ps1
@@ -1,7 +1,7 @@
 $scoop_shims="$(Resolve-Path "$(scoop prefix scoop)\..\..\..\shims")"
 $persist_dir="$(split-path $scoop_shims)\persist"
 $ssh_agent="$(split-path -Path "$scoop_shims")\apps\git\current\usr\bin\ssh-agent.exe";
-$conemu="$scoop_shims\ConEmu64.exe";
+$git_bash="$scoop_shims\git-bash.exe";
 $Shell = New-Object -ComObject ("WScript.Shell");
 $ShortCut = $Shell.CreateShortcut($env:USERPROFILE + "\Desktop\startit.lnk");
 $ShortCut.TargetPath="PowerShell.exe";
@@ -9,6 +9,6 @@ $ShortCut.Arguments="-NoProfile -ExecutionPolicy RemoteSigned `"$($persist_dir)\
 $ShortCut.WorkingDirectory=$(split-path $scoop_shims);
 $ShortCut.WindowStyle = 1;
 $ShortCut.Hotkey = "CTRL+ALT+SHIFT+F12";
-$ShortCut.IconLocation = $conemu + ", 0";
-$ShortCut.Description = "Start ConEmu with shared ssh-agent"
+$ShortCut.IconLocation = "$(Split-Path $ssh_agent)\mintty.exe,0"
+$ShortCut.Description = "Start Git-Bash with shared ssh-agent"
 $ShortCut.Save()
