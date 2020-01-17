aff8c951209e78739e78b61b38ba4059dff51dd2
Philippe Crama <pcfeb0009@gmx.com>
2020-01-17 21:18:42 +0100

    Start describing setup & installation of MSYS2 / Scoop mix

--- _general/makeshortcut.ps1
+++ _general/makeshortcut.ps1
@@ -1,14 +1,11 @@
-$scoop_shims="$(Resolve-Path "$(scoop prefix scoop)\..\..\..\shims")"
-$persist_dir="$(split-path $scoop_shims)\persist"
-$ssh_agent="$(split-path -Path "$scoop_shims")\apps\git\current\usr\bin\ssh-agent.exe";
-$conemu="$scoop_shims\ConEmu64.exe";
+$persist_dir=Resolve-Path "$(scoop prefix scoop)\..\..\..\persist"
 $Shell = New-Object -ComObject ("WScript.Shell");
 $ShortCut = $Shell.CreateShortcut($env:USERPROFILE + "\Desktop\startit.lnk");
 $ShortCut.TargetPath="PowerShell.exe";
-$ShortCut.Arguments="-NoProfile -ExecutionPolicy RemoteSigned `"$($persist_dir)\_general\starter.ps1`""
-$ShortCut.WorkingDirectory=$(split-path $scoop_shims);
+$ShortCut.Arguments="-NoProfile -ExecutionPolicy RemoteSigned `"$persist_dir\_general\starter.ps1`""
+$ShortCut.WorkingDirectory=$env:USERPROFILE;
 $ShortCut.WindowStyle = 1;
 $ShortCut.Hotkey = "CTRL+ALT+SHIFT+F12";
-$ShortCut.IconLocation = $conemu + ", 0";
-$ShortCut.Description = "Start ConEmu with shared ssh-agent"
+$ShortCut.IconLocation = "c:\msys64\usr\bin\mintty.exe,0";
+$ShortCut.Description = "Start minTTY"
 $ShortCut.Save()
--- _general/starter.ps1
+++ _general/starter.ps1
@@ -39,13 +39,15 @@ $scriptName = & { $myInvocation.ScriptName }
 $Env:SCOOP = Split-Path -Path (Split-Path -Path (Split-Path -Path (Resolve-Path $scriptName) -Parent) -Parent) -Parent
 Write-Host "SCOOP=`"$($Env:SCOOP)`""
 $Env:PATH = ($Env:PATH).Replace("%SCOOP%", $Env:SCOOP)
-Write-Host "Initial Path=$($Env:PATH)"
+# Even though msys2 will set HOME anyway, already prepare it in the
+# environment in case tools are started from e.g. multicommander:
+$Env:HOME = $Env:USERPROFILE
 
-$Env:HOME = $Env:HOMEDRIVE + $Env:HOMEPATH
+Write-Host "HOME='$Env:HOME'`nInitial Path=$(($Env:PATH).Replace(";", "`n`t"))"
+
 $opt = $Env:HOME + "\opt"
 $Env:PATH = (
-    "$($Env:SCOOP)\apps\emax64\current\emax64-pdumper\bin" +
-    ";$opt\bin" +
+    "$opt\bin" +
     ";$($Env:HOME)\AppData\Roaming\Python\Scripts" +
     ";C:\VASCO\Programs\Vasco.Tim.Runner\Vasco.Tim.Runner_2_4_1_7" +
     ";C:\VASCO\Programs\DPEmulator\DPEmulator_1_0_5_1_forVC_3_15_0_beta2" +
@@ -57,21 +59,7 @@ $Env:PATH = (
 
 $Env:GRAPHVIZ_DOT = $Env:SCOOP + "\shims\dot.exe"
 
-$basename = "git-bash.exe"
-$scoop = $Env:SCOOP
-$gitbash = "$scoop\apps\git\current\$basename"
-$ssh_agent = "$scoop\apps\git\current\usr\bin\ssh-agent.exe"
-Write-Host "SCOOP=`"$($Env:SCOOP)`"`ngitbash=`"$gitbash`"`nbasename=`"$basename`"`nscoop=`"$scoop`"`nssh_agent=`"$ssh_agent`""
-# Thanks to https://superuser.com/questions/1104567/how-can-i-find-out-the-command-line-options-for-git-bash-exe
-Start-Process -WindowStyle Hidden -FilePath "$ssh_agent" -ArgumentList @("$gitbash", "--cd-to-home")
-
-# [2019-10-21]: Switch back to MultiCommander...
-#
-# # Persisting DoubleCmd config doesn't work: DoubleCmd rewrites the file, thus
-# # losing the hard link, so simulate by copying newer files over older files
-# # (NB: this doesn't handle conflicts nicely):
-# robocopy "$scoop\persist\doublecmd\" "$scoop\apps\doublecmd\current" history.xml doublecmd.xml /XO /V
-# robocopy "$scoop\apps\doublecmd\current" "$scoop\persist\doublecmd\" history.xml doublecmd.xml /XO /V
+Start-Process -WindowStyle Hidden -FilePath "c:\msys64\msys2_shell.cmd" -ArgumentList @("-mingw64", "-mintty", "-where", $Env:USERPROFILE, "-use-full-path", "-shell", "bash")
 
 Wait-ProcessExistsP bash
 
